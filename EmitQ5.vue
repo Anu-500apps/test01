2e329c8a3c921cf4fb5247ab590c422a9c4e5c26
Anu-500apps <102018575+Anu-500apps@users.noreply.github.com>
2022-03-31 18:55:44 +0530

    Add files via upload

---
+++ EmitQ5.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+      <h1>This is Emit</h1>
+    <h2>email:{{ name }}</h2>
+
+    <input
+      type="text"
+      placeholder="enter the text"
+      @change="$emit('userdetails', $event.target.value)"
+    />
+  </div>
+</template>
+
+<script>
+export default {
+  name: "EmiT",
+
+  methods: {
+    userdetails(event) {
+      this.$emit("anuradha", event.target.value);
+    },
+  },
+};
+</script>
+
+
+
---
+++ btable.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <b-table striped hover :items="items" :fields="fields"></b-table>
+  </div>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        fields:['iso3','name'],
+        items: [{ name: 'Barbados', iso3: 'BRB' },
+  { name: 'Brunei Darussalam', iso3: 'BRN' },
+  { name: 'Bhutan', iso3: 'BTN' },
+  { name: 'Bouvet Island', iso3: 'BVT' },
+  { name: 'Botswana', iso3: 'BWA' },
+  { name: 'Central African Republic', iso3: 'CAF' },
+  { name: 'Canada', iso3: 'CAN' },
+  { name: 'Cocos (Keeling) Islands', iso3: 'CCK' },
+  { name: 'Switzerland', iso3: 'CHE' },
+  { name: 'Chile', iso3: 'CHL' },
+  { name: 'China', iso3: 'CHN' },
+  { name: "Côte d'Ivoire", iso3: 'CIV' },
+  { name: 'Cameroon', iso3: 'CMR' },
+  { name: 'Congo, Democratic Republic of the', iso3: 'COD' },
+  { name: 'Congo', iso3: 'COG' },
+  { name: 'Cook Islands', iso3: 'COK' },
+  { name: 'Colombia', iso3: 'COL' },]
+      }
+    }
+  }
+</script>
